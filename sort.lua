956072cc5ad8f4391da8feec4dfcb91988db413a
deanturpin <git@lvbtn.uk>
2016-10-12 17:15:17 +0100

    halve data set

--- sort.lua
+++ sort.lua
@@ -3,18 +3,24 @@
 require "utils"
 
 -- A random sequence
-sequence = {
+local sequence = {
 	13, 6, 30, 17, 6, 28, 25, 22, 13, 24, 29, 9, 6, 21, 12, 16, 11, 16, 11, 12,
-	1, 31, 3, 15, 12, 10, 18, 11, 13, 28, 10, 28, 21, 5, 27, 22, 12, 26, 24, 8
+	-- 1, 31, 3, 15, 12, 10, 18, 11, 13, 28, 10, 28, 21, 5, 27, 22, 12, 26, 24, 8
 }
 
-histogram(sequence)
-
 -- Insertion sort
-function insertionSort(s)
+function sequence:sort()
 
-	for key, value in pairs(sequence) do
+	for key, value in ipairs(self) do
+		
+		-- Clear value
+		-- self[1] = 0
+
+		-- Print sequence
+		histogram(self)
 	end
 end
+
+sequence:sort()
 
 io.write("C'est fini")
--- utils.lua
+++ utils.lua
@@ -14,12 +14,23 @@ function bar(n)
 	else return "-" .. bar(n - 1) end
 end
 
+calls = 0
+
 -- Print a sequence as a histogram
 function histogram(h)
 
+	calls = calls +1
+
+	-- Clear the screen
+	os.execute("clear")
+
 	-- For each value in the sequence print a bar to construct histogram
-	for key, value in pairs(sequence) do
+	for key, value in ipairs(h) do
 
 		io.write(bar(value), "\n")
 	end
+
+	print("Calls", calls)
+
+	sleep(0.05);
 end
